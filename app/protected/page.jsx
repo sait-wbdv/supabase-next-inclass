75581fc1e2570989bfd36503639c775df4d21582
Ashlyn Knox <ashlyn@lilyx.io>
2024-12-05 10:46:28 -0700

    protected page code setup

---
+++ app/protected/page.jsx
@@ -0,0 +1,16 @@
+import { redirect } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+
+export default async function PrivatePage() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+  return (
+    <main>
+      <h1>This is a protected page</h1>
+      <p>Only authenticated users should be able to see it</p>
+    </main>
+  );
+}
