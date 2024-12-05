6e3033dfc47d4f98c80f58d43206a5ddf66dce86
Ashlyn Knox <ashlyn@lilyx.io>
2024-12-05 09:18:11 -0700

    supabase middleware

---
+++ utils/supabase/middleware.js
@@ -0,0 +1,51 @@
+// imports
+import { createServerClient } from "@supabase/ssr";
+import { NextResponse } from "next/server";
+
+// update session async function
+export async function updateSession(request) {
+  let supabaseResponse = NextResponse.next({
+    request,
+  });
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            request.cookies.set(name, value)
+          );
+          supabaseResponse = NextResponse.next({
+            request,
+          });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            supabaseResponse.cookies.set(name, value, options)
+          );
+        },
+      },
+    }
+  );
+
+  // get user
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  // error handle if someone tries to go to pages they're not suposed to, they will be redirected to login
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith("/login") &&
+    !request.nextUrl.pathname.startsWith("/auth")
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
+    const url = request.nextUrl.clone();
+    url.pathname = "/login";
+    return NextResponse.redirect(url);
+  }
+  // return the response
+  return supabaseResponse;
+}
